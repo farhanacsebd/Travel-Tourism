6e82f85b3fff7f948c5735e604434768d4a36019
faisalcsebd <farhana.cse.bd@gmail.com>
2021-11-04 18:14:34 +0600

    banner hight or covid

--- src/component/Home/Banner/Covi.js
+++ src/component/Home/Banner/Covi.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Covid = () => {
     return (
         <div>
-            <marquee width="100%" direction="left" height="32px" style={{ color: "red" }} className="fs-3">
+            <marquee width="100%" direction="left" height="40px" style={{ color: "red" }} className="fs-3">
                 Get the advice you during Covid situation, Must follow the latest COVID-19 restrictions when you are traveling.Any times to need then call us 333666 number.
             </marquee>  </div>
     );
